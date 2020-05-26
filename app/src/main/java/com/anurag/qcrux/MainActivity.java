a5d09b50f0b707349f968ef6fa75374e584e488b
Anurag Pal <55744578+Anuragtech02@users.noreply.github.com>
2020-05-26 16:37:06 +0530

    Create post layout complete

--- app/src/main/java/com/anurag/qcrux/MainActivity.java
+++ app/src/main/java/com/anurag/qcrux/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.post_selected);
+        setContentView(R.layout.new_post);
     }
 }
