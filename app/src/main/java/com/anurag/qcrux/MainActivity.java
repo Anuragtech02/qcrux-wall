cd49654d583b4e1f8a09c3c06f1fc9126cbcc1c2
Anurag Pal <55744578+Anuragtech02@users.noreply.github.com>
2020-05-27 14:06:16 +0530

    People you may know layout complete

--- app/src/main/java/com/anurag/qcrux/MainActivity.java
+++ app/src/main/java/com/anurag/qcrux/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_post);
+        setContentView(R.layout.activity_people_you_know);
     }
 }
