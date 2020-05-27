e58c64fed59d497cf37f05bd24ad4994204423da
Anurag Pal <55744578+Anuragtech02@users.noreply.github.com>
2020-05-27 15:52:17 +0530

    Invite friends layout done

--- app/src/main/java/com/anurag/qcrux/MainActivity.java
+++ app/src/main/java/com/anurag/qcrux/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_people_you_know);
+        setContentView(R.layout.activity_invite);
     }
 }
