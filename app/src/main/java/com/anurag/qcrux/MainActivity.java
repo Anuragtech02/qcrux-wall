f3897c2af39fb0a4a5f73e128c0079dcf446f0b9
Anurag Pal <55744578+Anuragtech02@users.noreply.github.com>
2020-05-29 11:27:34 +0530

    Layout interested in complete

--- app/src/main/java/com/anurag/qcrux/MainActivity.java
+++ app/src/main/java/com/anurag/qcrux/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interest);
+        setContentView(R.layout.activity_interested_in);
     }
 }
